7d608416243b55620db57dae0161932a94d9ef6d
Dinh Tran <trandtb@greenglobal.vn>
2019-08-30 11:31:44 +0700

    add missing querydsl-apt in compile plugin

--- src/main/java/vn/tcx/app/repository/UserRepository.java
+++ src/main/java/vn/tcx/app/repository/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.tcx.app.entity.User;
